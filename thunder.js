c38e02bfd5946d612a612e295d587df11fd54cbe
naveenkumar68775 <naveenkumar68775@gmail.com>
2026-08-30 16:29:59 +0530

    God

---
+++ thunder.js
@@ -0,0 +1,54 @@
+// a=10;
+// b=20;
+// console.log(a+b);
+
+// var  a=20;
+// var b=21;
+// let c=a+b;
+
+// if(a!=b){
+//     console.log(true)
+// }
+// else{
+//     console.log("Incorrect");
+//     }
+
+// console.log(c)
+
+// var a="Constructor";
+// console.log(a)
+
+// a=10;
+// // a++;
+// console.log(a++);
+// a=a++;
+// ++a;
+
+
+// console.log(a);
+
+// var a=10;
+// console.log(a++ + ++a/10);
+
+// console.log(type(true));
+
+// var a=null;
+// a=true
+// console.log(a);
+
+// console.log(type(true));
+
+// function thunder(a,b){
+//     // a=10;b=20;
+//     c=a+b;
+//     console.log(c);
+// }
+// thunder(10,20);
+
+// for(v=1;v<=5;v++){
+//     console.log(v);
+// }
+
+for(v=1;v<=10;v++){
+    console.log("1x2=", v*2)
+}
